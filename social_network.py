1e9c4b8c198aa0945a4d96e7e4bf0be04c6be0d1
Valentina Benitez <valbenitez@valentinasair4.devices.brown.edu>
2023-07-18 14:53:32 -0400

    Submitted

--- social_network.py
+++ social_network.py
@@ -3,9 +3,10 @@ from  social_network_classes import SocialNetwork,Person
 import social_network_ui
 
 
-
+person = Person(0, 0)
 #Create instance of main social network object
 ai_social_network = SocialNetwork()
+ai_social_network.add_current_user(person)
 
 #The line below is a python keyword to specify which 
 if __name__ == "__main__":
@@ -19,18 +20,55 @@ if __name__ == "__main__":
         if choice == "1":
             print("\nYou are now in the create account menu")
             ai_social_network.create_account()
+            
 
         elif choice == "2":
+            username = input("What's your username? ") 
+            current_user = ai_social_network.add_current_user(username)
             inner_menu_choice = social_network_ui.manageAccountMenu()
-            #Handle inner menu here
+            # Handle inner menu here
             while True:
-                if inner_menu_choice == "5":
+                if inner_menu_choice == "1":
+                    print("Change username")
+                    ai_social_network.current_user.name = input("Write your new username: ")
+                    ai_social_network.current_user.age = input("Write your new age: ")
                     break
-                else:
-                    inner_menu_choice = social_network_ui.manageAccountMenu()
+
+                elif inner_menu_choice == "2":
+                    #print("the name is: ", ai_social_network.current_user.name)
+                    #print("the name ois age: ", ai_social_network.current_user.age)
+                    addedfriend = input("What is your friends name?  ")
+                    current_user.add_friend(addedfriend)
+                    # ai_social_network.current_user.
+                    break
+                
+                elif inner_menu_choice == "3":
+                    current_user.view_friends()
+                    break
+
+                elif inner_menu_choice == "4":
+                    friend_name = input ("Enter the name of the person you want to contact: ")
+                    friend = ai_social_network.add_current_user(friend_name)
+                    current_user.send_message(friend)
+                    
+                elif inner_menu_choice == "8":
+                    current_user.view_messages()
+                    
+
+                elif inner_menu_choice == "5":
+                    break
+                elif inner_menu_choice == "6":
+                    username = input("Who would you like to block? ")
+                    current_user.block_friend(username)
+                    break
+                elif inner_menu_choice == "7":
+                    current_user.view_blocklist()
+                    break
+    
+                inner_menu_choice = social_network_ui.manageAccountMenu()
 
         elif choice == "3":
-            print("Thank you for visiting. Goodbye3")
+            print("Thank you for visiting. Goodbye")
             break
 
         else:
--- social_network_classes.py
+++ social_network_classes.py
@@ -2,9 +2,15 @@
 class SocialNetwork:
     def __init__(self):
         self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
-                                 # you can save objects of people on the network in this list
+                            # you can save objects of people on the network in this list
         
     ## For more challenge try this
+
+    def add_current_user(self, current_user):
+        for person in self.list_of_people:
+            if person.id == current_user:
+                return person
+
     def save_social_media(self):
         # function to save social media to a file on disk 
         # hint: look up how to use python's inbuil json module to turn objects to json
@@ -20,7 +26,20 @@ class SocialNetwork:
     def  create_account(self):
         #implement function that creates account here
         print("Creating ...")
+        name = input("Enter your name: ")
+        age = input("Enter your age: ")
+        user = Person(name, age)
+        self.list_of_people.append(user)
+        return user
+    
+    def send_message(person,message):
+        #implement sending message to friend here
         pass
+    
+
+    
+        
+
 
 
 class Person:
@@ -28,11 +47,31 @@ class Person:
         self.id = name
         self.year = age
         self.friendlist = []
+        self.blocklist = []
+        self.messages = []
 
     def add_friend(self, person_object):
         #implement adding friend. Hint add to self.friendlist
-        pass
+        self.friendlist.append(person_object)
+        print(self.friendlist)
+       
 
-    def send_message(self):
-        #implement sending message to friend here
-        pass
+    def view_friends(self):
+        print(self.friendlist)
+
+    def view_messages(self):
+        print(self.messages)
+
+    def block_friend(self, person_object):
+        self.blocklist.append(person_object)
+        
+
+    def view_blocklist(self):
+        print("Current blocked users:")
+        print(self.blocklist)
+
+    def send_message(self, person_object):
+        message = input("Enter your message: ")
+        person_object.messages.append(message)
+        print("Message sent!")
+
